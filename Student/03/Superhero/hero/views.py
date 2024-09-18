7d6bbea4bd9f76a70e1611bbc7bf3e33d3d06ed8
Kevin S <kevin@sirbomble.com>
2024-09-18 22:36:23 +0000

    Project 3

--- Student/03/Superhero/hero/views.py
+++ Student/03/Superhero/hero/views.py
@@ -1,38 +1,57 @@
 from django.views.generic import TemplateView
 
+heroes = {
+    'hulk': {
+        'title': 'Hulk',
+        'body': 'My name is Bruce Banner',
+        'image': '/static/images/hulk.jpg',
+        'url': '/hulk'
+    },
+    'ironman': {
+        'title': 'Iron Man',
+        'body': 'My name is Tony Stark, but I am Iron Man',
+        'image': '/static/images/iron_man.jpg',
+        'url': '/ironman'
+    },
+    'blackwidow': {
+        'title': 'Black Widow',
+        'body': 'My name is Natasha Romanova',
+        'image': '/static/images/black_widow.jpg',
+        'url': '/blackwidow'
+    },
+    'spiderpig': {
+        'title': 'Spider Pig',
+        'body': 'My name is Peter Porker',
+        'image': '/static/images/spiderpig.jpg',
+        'url': '/spiderpig'
+    }
+}
 
 class IndexView(TemplateView):
     template_name = 'heroes.html'
-
 
 class HulkView(TemplateView):
     template_name = 'hero.html'
 
     def get_context_data(self, **kwargs):
-        return {
-            'title': 'Hulk',
-            'body': 'My name is Bruce Banner',
-            'image': '/static/images/hulk.jpg'
-        }
+        return heroes['hulk']
 
 
 class IronManView(TemplateView):
     template_name = "hero.html"
 
     def get_context_data(self, **kwargs):
-        return {
-            'title': 'Iron Man',
-            'body': 'My name is Tony Stark, but I am Iron Man',
-            'image': '/static/images/iron_man.jpg'
-        }
+        return heroes['ironman']
 
 
 class BlackWidow(TemplateView):
     template_name = 'hero.html'
 
     def get_context_data(self, **kwargs):
-        return {
-            'title': 'Black Widow',
-            'body': 'My name is Natasha Romanova',
-            'image': '/static/images/black_widow.jpg'
-        }
+        return heroes['blackwidow']
+
+class SpiderPig(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return heroes['spiderpig']
