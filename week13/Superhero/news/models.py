9da4b72275c23bfff52b7383333461ba168a945e
Kevin S <kevin@sirbomble.com>
2024-11-22 23:14:58 +0000

    Project 12

--- week13/Superhero/news/models.py
+++ week13/Superhero/news/models.py
@@ -39,6 +39,20 @@ class Article (models.Model):
 
     def get_absolute_url(self):
         return reverse_lazy('article_detail', args=[str(self.id)])
+    
+def get_upload(instance, filename):
+        return f'images/{filename}'
+
+class Photo(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(null=True, blank=True, upload_to=get_upload)
+    uploaded_by = models.ForeignKey(
+        'auth.User', on_delete=models.CASCADE, related_name='photos'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name='photos', null=True, blank=True
+    )
 
 class Superhero(models.Model):
     name = models.CharField(max_length=200)
@@ -48,6 +62,10 @@ class Superhero(models.Model):
     weakness = models.CharField(max_length=100, default='')
     image = models.CharField(max_length=100, default='')
     investigator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='superheroes')
-    
+    photos = models.ManyToManyField(Photo, blank=True, related_name='superheroes')
+
+    def add_photo(self, photo):
+        self.photos.add(photo)
+        
     def __str__(self):
         return self.name
